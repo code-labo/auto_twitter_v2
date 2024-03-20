733da9dc29a1c66b56e14aa629971d622b2b5b14
code-labo <code.lab.200011@gmail.com>
2024-03-20 16:25:43 +0900

    fix: get_followers_tweet@sql->csv

--- app/get_followers_tweet.py
+++ app/get_followers_tweet.py
@@ -9,70 +9,38 @@ ROOT=str(Path(__file__).parent.parent)
 sys.path.append(ROOT)
 
 import numpy as np
-from math import floor
-import psycopg2
 from datetime import datetime
+import pandas as pd
 
 from src.auto_twitter import AutoTwitter
 from src.filters import date_filter
 from src.envs import *
-from src.utils import add_single_quote
+from src.utils import Color
 
 
 def main():
 
     num_account=GET_FOLLOWERS_TWEET_CFG["NUM_ACCOUNT"]
-    now_datetime=datetime.now()
+    now=datetime.now()
+
+    accounts=pd.read_csv(f"{PARENT}/database/accounts.csv")
+    followers=accounts[accounts["is_followed"]==True]
+    target_followers=followers.sort_values(by="access_at",ascending=True).iloc[:num_account] #昇順に並び替え
+
+    msg="-"*40+f"{Color.MAGENTA}target followers{Color.RESET}"+"-"*40
+    print(f"{msg}\n{target_followers}\n{'-'*(len(msg)-len(f'{Color.CYAN}{Color.RESET}'))}")
+
 
     base_url="https://twitter.com"
-    auto_twitter=AutoTwitter(driver_name="msedgedriver.exe")
+    auto_twitter=AutoTwitter()
     auto_twitter.access_url(url=base_url)
 
 
-    ##データベースへの接続
-    conn=psycopg2.connect(
-        host=HOST,user=USER,password=PASSWORD,database=DATABASE
-    )
-
-    with conn:
-        with conn.cursor() as cursor:
-
-            #最大idの取得.自動で振り分けされない.なんでや
-            query="SELECT MAX(id) FROM tweet;"
-            cursor.execute(query)
-            result=cursor.fetchone()
-            id_last=int(result[0]) if not result[0] is None else 0
-            #
-            
-            #アカウントのurlを取得
-            except_account_query="("+",".join(map(add_single_quote,EXCEPT_ACCOUNT))+")"
-            query=f"""SELECT id,name 
-                     FROM account
-                     WHERE roll='follower'
-                     AND NOT name IN {except_account_query}
-                     ORDER BY favo_at ASC
-                     LIMIT {num_account};"""
-            print(query)
-            cursor.execute(query)
-            result=np.array(cursor.fetchall())
-
-            if len(result)>0:
-                account_ids=result[:,0].flatten()
-                account_names=[account_name.replace(" ","") for account_name in result[:,1].flatten()]
-            #
-        conn.commit()
-    print(result)
-    if len(result)==0:
-        print("***No Follower***")
-        exit(1)
-
-
-    #1つのアカウントにつき最新のツイートを1つ取ってくる
-    idx=1
+    #>> 1つのアカウントにつき最新のツイートを1つ取ってくる (あれば) >>
     register_values=[] #DBに挿入するデータリスト
-    for account_id,account_name in zip(account_ids,account_names):
+    for account_id,account_record in target_followers.iterrows():
 
-        account_url=base_url+"/"+account_name
+        account_url=base_url+"/"+account_record["name"]
         auto_twitter.access_url(account_url) #アカウントページにアクセス
         tweet_url=[]
         current_height=0
@@ -85,24 +53,34 @@ def main():
             scroll_num+=1
         
         if len(tweet_url)>0:
-            register_values+=[[idx+id_last,account_id,tweet_url.pop(0),now_datetime]]
-            idx+=1
+            register_values+=[[account_record["name"],tweet_url.pop(0),3.0]]
     print(register_values)
-    #
+    #>> 1つのアカウントにつき最新のツイートを1つ取ってくる (あれば) >>
 
     if len(register_values)==0: #新規tweetが無ければおしまい
         print("***No New Tweet...***")
         exit(1)
-    
 
-    #データベースに登録
-    with conn:
-        with conn.cursor() as cursor:
-            query="""INSERT INTO tweet
-                     (id,account_id ,url, get_time) VALUES(%s,%s,%s,%s);
-                  """
-            cursor.executemany(query,register_values)
-        conn.commit()
+    #>> accountテーブルのアクセス時間の更新 >>
+    for record_idx, record in target_followers.iterrows():
+        accounts.at[record_idx,"access_at"]=now
+    accounts.to_csv(f"{PARENT}/database/accounts.csv",index=False,encoding="utf-8")
+    #>> accountテーブルのアクセス時間の更新 >>
+    
+    #>> tweetテーブルに新規追加 >>
+    new_tweets=pd.DataFrame(
+        register_values,columns=["account","url","priority"]
+    )
+    tweets=pd.read_csv(f"{PARENT}/database/tweets.csv",encoding="utf-8")
+    tweets=pd.concat(
+        [tweets,new_tweets]
+    )
+    tweets.drop_duplicates(inplace=True) #一応重複を消す
+    tweets.to_csv(f"{PARENT}/database/tweets.csv",index=False,encoding="utf-8")
+
+    msg="-"*40+f"{Color.CYAN}new tweets{Color.RESET}"+"-"*40
+    print(f"{msg}\n{new_tweets}\n{'-'*(len(msg)-len(f'{Color.CYAN}{Color.RESET}'))}")
+    #>> tweetテーブルに新規追加 >>
 
 if __name__=="__main__":
     main()
