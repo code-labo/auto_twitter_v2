40baf745c2ecc26e8a53e518f08a3807538f4ba9
code-labo <code.lab.200011@gmail.com>
2024-03-20 15:45:58 +0900

    fix: get_follower@data io algorithm

--- app/get_follower.py
+++ app/get_follower.py
@@ -8,60 +8,76 @@ PARENT=str(Path(__file__).parent)
 ROOT=str(Path(__file__).parent.parent)
 sys.path.append(ROOT)
 
-import psycopg2
-import numpy as np
 import datetime
 import pandas as pd
 
 from src.auto_twitter import AutoTwitter
 from src.envs import *
+from src.utils import Color
 
 def main():
 
     max_minutes=GET_FOLLOWER_CFG["max_minutes"]
 
 
-    account_db=pd.read_csv(f"{PARENT}/database/accounts.csv") #既に登録したアカウントを読み込み
-
-
-    #>> フォロワーの取得 >>
     url="https://twitter.com"
     auto_twitter=AutoTwitter()
     auto_twitter.access_url(url)
     
-    follower_link_list=auto_twitter.get_follower_links(
-        account_url=f"{url}/{ACCOUNT_NAME}",max_minutes=max_minutes
-    )
 
-    new_follower=[]
-    for link in follower_link_list:
+    #>> フォロワー名の取得 >>
+    obtained_accounts=[
+        link.replace("https://twitter.com/","") for link
+        in auto_twitter.get_follower_links(
+                account_url=f"{url}/{ACCOUNT_NAME}",max_minutes=max_minutes
+            )
+    ]
+    #>> フォロワー名の取得 >>
 
-        name=link.replace("https://twitter.com/","")
-        if len(account_db[account_db["name"]==name])==0: #まだ登録されていないとき
-            access_at=datetime.datetime.now()-datetime.timedelta(days=365*10) #まだアクセスしてないので, すごく昔の時間を登録しとく
-            is_followed=True
-            is_following=False
-            is_favo=False
-            new_follower+=[[
-                name,is_following,is_followed,
-                is_favo,access_at
-            ]]
-        elif len(account_db[account_db["name"]==name])>0: #登録済みのとき
-            pass
-    #>> フォロワーの取得 >>
+
+    accounts=pd.read_csv(f"{PARENT}/database/accounts.csv",encoding="utf-8") #既に登録されたアカウントを読み出し
     
 
-    #>> 登録 >>
-    new_follower_db=pd.DataFrame(
-        new_follower,columns=["name","is_following","is_followed","is_favo","access_at"]
+    #>> 既に持ってるアカウントのis_follwedをTrueに >>
+    owned_accounts=accounts[
+        (accounts["name"].isin(obtained_accounts))
+    ]
+    for record_idx,record in owned_accounts.iterrows():
+        accounts.at[record_idx,"is_followed"]=True
+    msg="-"*40+f"{Color.CYAN}update accounts{Color.RESET}"+"-"*40
+    print(f"{msg}\n{owned_accounts}\n{'-'*(len(msg)-len(f'{Color.CYAN}{Color.RESET}'))}")
+    #>> 既に持ってるアカウントのis_followedをTrueに >>
+
+
+    #>> 新しいアカウントを追加 >>
+    new_accounts=[]
+    for account in obtained_accounts:
+        if owned_accounts.shape[0]>0:
+            if not account in owned_accounts["name"].values:
+                access_at=datetime.datetime.now()-datetime.timedelta(days=365*10) #まだアクセスしてないので, すごく昔の時間を登録しとく
+                new_accounts+=[
+                    [account,False,True,False,access_at]
+                ]
+        else:
+            access_at=datetime.datetime.now()-datetime.timedelta(days=365*10) #まだアクセスしてないので, すごく昔の時間を登録しとく
+            new_accounts+=[
+                [account,False,True,False,access_at]
+            ]
+    new_accounts=pd.DataFrame(
+        new_accounts,
+        columns=["name","is_following","is_followed","is_favo","access_at"]
     )
-    print("new followers"+"-"*50)
-    print(new_follower_db)
-    account_db=pd.concat(
-        [account_db,new_follower_db]
+    msg="-"*40+f"{Color.MAGENTA}new accounts{Color.RESET}"+"-"*40
+    print(f"{msg}\n{new_accounts}\n{'-'*(len(msg)-len(f'{Color.CYAN}{Color.RESET}'))}")
+    #>> 新しいアカウントを追加 >>
+
+
+    #>> テーブルにnew_accountを追加して保存 >>
+    accounts=pd.concat(
+        [accounts,new_accounts]
     )
-    account_db.to_csv(f"{PARENT}/database/accounts.csv",index=False,encoding="utf-8")
-    #>> 登録 >>
+    accounts.to_csv(f"{PARENT}/database/accounts.csv",index=False,encoding="utf-8")
+    #>> テーブルにnew_accountを追加して保存 >>
 
 
 if __name__=="__main__":
